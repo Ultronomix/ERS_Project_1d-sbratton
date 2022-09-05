b8e2dd8f77cb1ccd321d66748d83edf435dbcdd9
cameramanbrayton <info@duelingworlds.com>
2022-09-05 18:48:45 -0500

    Activate Post/ for data retrevial, Error Db not recognizing WHERE

--- project-0c-sbratton/src/main/java/com/p0a/com/cameramanbrayton/workers/auth/AuthServlet.java
+++ project-0c-sbratton/src/main/java/com/p0a/com/cameramanbrayton/workers/auth/AuthServlet.java
@@ -1,9 +1,11 @@
 package main.java.com.p0a.com.cameramanbrayton.workers.auth;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import main.java.com.p0a.com.cameramanbrayton.workers.users.User;
 import main.java.com.p0a.com.cameramanbrayton.workers.users.UserDAO;
 
 import java.io.IOException;
@@ -19,7 +21,14 @@ public class AuthServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //super.doPost(req, resp);
-        resp.getWriter().write("/auth works!");
+        //resp.getWriter().write("/auth works!");
+        ObjectMapper jsonMapper = new ObjectMapper();
+        resp.setContentType("application/json");
+        Credentials credentials = jsonMapper.readValue(req.getInputStream(), Credentials.class);
+        User loggedInUser  = userDAO.findUserByUsernameAndPassword(credentials.getUsername(), credentials.getPassword())
+                .orElseThrow(() -> new RuntimeException("No user found with those Credentials!"));
+
+        resp.getWriter().write(jsonMapper.writeValueAsString(loggedInUser));
     }
 
     public UserDAO getUserDAO() {
