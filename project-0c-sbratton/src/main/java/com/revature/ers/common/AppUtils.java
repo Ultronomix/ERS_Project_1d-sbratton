4dc5a82708f27c20187e8618581b5db0d41f5364
cameramanbrayton <info@duelingworlds.com>
2022-09-18 21:28:38 -0500

    Storm coming, saving prior

--- project-0c-sbratton/src/main/java/com/revature/ers/common/AppUtils.java
+++ project-0c-sbratton/src/main/java/com/revature/ers/common/AppUtils.java
@@ -9,21 +9,9 @@ public class AppUtils {
         try {
             return Integer.parseInt(possibleInt);
         } catch (NumberFormatException e) {
+            e.printStackTrace();
             throw new InvalidRequestException("A non-numeric id value was provided!");
+
         }
     }
-
-    /*public static boolean isAdmin(UserResponse subject) {
-
-    }*/
-
-    /*public static String parseInt(int possibleInt) {
-        try {
-            return Integer.parseInt(possibleInt);
-            //(Integer)(possibleInt).t
-        } catch (NumberFormatException e) {
-            throw new InvalidRequestException("A non-numeric id value was provided!");
-        }
-    }*/
-
 }
